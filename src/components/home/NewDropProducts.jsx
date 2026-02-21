bb8e3d899070b219e9d9af27cab920dcb37e56be
rajibulislam60 <rajibulislam60707877@gmail.com>
2026-02-21 23:16:16 +0600

    newdrp responsive done

--- src/components/home/NewDropProducts.jsx
+++ src/components/home/NewDropProducts.jsx
@@ -11,18 +11,24 @@ const NewDropProducts = () => {
     <div className="mt-[90px] mb-[128px]">
       <Container>
         <div>
-          <div className="relative overflow-hidden mb-8">
-            <h2 className="text-[74px] font-semibold w-[589px] leading-[95%]">
+          <div className="flex justify-between items-center lg:relative overflow-hidden mb-8">
+            <h2 className="text-[24px] lg:text-[74px] font-semibold w-[172px] lg:w-[589px] leading-[95%]">
               Don't miss out new drops
             </h2>
-            <button className="px-8 py-4 uppercase text-[14px] font-medium text-white bg-[#4A69E2] absolute bottom-0 right-0 rounded-[8px]">
+            <button className="px-7 lg:px-8 py-3 lg:py-4 uppercase text-[14px] font-medium text-white bg-[#4A69E2] lg:absolute bottom-0 right-0 rounded-[8px]">
               Shop New Drops
             </button>
           </div>
-          <div className="grid grid-cols-2 lg:grid-cols-4 gap-5">
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
             {products.slice(0, 4).map((product) => (
               <div key={product.id}>
-                <img src={product.images[0]} alt={product.title} />
+                <div className="">
+                  <img
+                    src={product.images[0]}
+                    alt={product.title}
+                    className="rounded-[12px] border-2 border-white object-cover"
+                  />
+                </div>
                 <h3 className="text-[24px] font-semibold my-4">
                   {product.title}
                 </h3>
